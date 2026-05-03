3f7248f14a70953deab8df339491921733444399
NAHIDA SULTANA <nahidas705@gmail.com>
2026-05-03 23:17:35 +0600

    added user delete

--- index.js
+++ index.js
@@ -94,6 +94,14 @@ async function run() {
       res.send(result)
     })
 
+     app.delete('/users/:id' , async(req,res)=>{
+      const id =req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
